b32de5e9a03cc0b776cdb4497043706ab7e58b0b
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-04-23 22:27:11 +0300

    create entities

---
+++ src/main/java/org/fomabb/demo/entity/EmailData.java
@@ -0,0 +1,26 @@
+package org.fomabb.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "email_data")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class EmailData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "id")
+    private User user;
+
+    @Column(nullable = false, length = 200, unique = true)
+    private String email;
+}
---
+++ src/main/java/org/fomabb/demo/entity/User.java
@@ -0,0 +1,34 @@
+package org.fomabb.demo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 500)
+    private String name;
+
+    @Column(name = "date_of_birth", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dateOfBirth;
+
+    @Column(nullable = false, length = 500)
+    @Size(min = 8, max = 500)
+    private String password;
+}
