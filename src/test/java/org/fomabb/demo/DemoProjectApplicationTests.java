5d445a2b6e96add8c30727f4c1d6d65c940f8651
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-04-24 18:25:10 +0300

    add test properties

--- src/test/java/org/fomabb/demo/DemoProjectApplicationTests.java
+++ src/test/java/org/fomabb/demo/DemoProjectApplicationTests.java
@@ -2,8 +2,10 @@ package org.fomabb.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.config.location=classpath:/application.properties")
 class DemoProjectApplicationTests {
 
     @Test
