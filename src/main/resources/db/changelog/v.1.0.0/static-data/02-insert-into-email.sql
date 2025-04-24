7c0f7818ed7e5ce6b4f80970c156b825f5c681f9
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-04-24 16:40:03 +0300

    add migration

---
+++ src/main/resources/db/changelog/v.1.0.0/static-data/02-insert-into-email.sql
@@ -0,0 +1,10 @@
+INSERT INTO email_data (USER_ID, EMAIL)
+VALUES (1, 'alice.smith@example.com'),
+       (1, 'alice.smith@gmail.com'),
+       (2, 'bob.johnson@example.com'),
+       (3, 'charlie.brown@example.com'),
+       (3, 'charlie.brown@yahoo.com'),
+       (3, 'charlie.brown@outlook.com'),
+       (4, 'diana.prince@example.com'),
+       (5, 'ethan.hunt@example.com'),
+       (5, 'ethan.hunt@gmail.com');
