e3e95f89bb1d1c661bfb875f56dcd843ac83c53d
Nikolay Kirilyuk <kirabb123@gmail.com>
2025-04-24 17:38:16 +0300

    add admin and logic for admin

---
+++ src/main/resources/db/changelog/v.1.0.0/static-data/05-insert-int-admin.sql
@@ -0,0 +1,15 @@
+INSERT INTO users (NAME, PRIMARY_EMAIL, DATE_OF_BIRTH, PASSWORD, role)
+VALUES ('admin',
+        'admin@gmail.com',
+        '1212-12-12',
+        '$2a$10$Ts70uPxEp1E1dFW4D/H1XeYdZAA7QdLLRUR/4bvO6ey/qYQ24DMZ6',
+        'ROLE_ADMIN');
+
+insert into phone_data (user_id, phone)
+values (6, '79121212121');
+
+insert into email_data (user_id, email)
+values (6, 'admin@gmail.com');
+
+insert into accounts (user_id, balance)
+values (6, '12.21');
